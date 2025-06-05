bbc6ef0c648d0903097abdda7d7fb543e740ba7b
Omar <sup4anski@gmail.com>
2025-06-05 12:14:38 +0500

    build fix

--- nfac-third-front-tg/src/main.tsx
+++ nfac-third-front-tg/src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import { AppRouter } from './router.tsx'
 
-const queryClient = new QueryClient()
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AppRouter />
-    </QueryClientProvider>
+    <AppRouter />
   </StrictMode>,
 )
