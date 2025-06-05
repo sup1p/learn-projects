94cf8b542b6e8f3783c01d49b443573587f97603
Omar <sup4anski@gmail.com>
2025-06-05 12:23:55 +0500

    fix deploy 2

--- nfac-third-front-tg/src/main.tsx
+++ nfac-third-front-tg/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import { AppRouter } from './router.tsx'
 
+// Create a client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      retry: 1,
+    },
+  },
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRouter />
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+    </QueryClientProvider>
   </StrictMode>,
 )
